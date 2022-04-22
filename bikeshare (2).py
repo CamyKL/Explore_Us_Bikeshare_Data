4ad2d931ecfb626d6ac27962d069597a8ef7ef7a
CamyKL <104034018+CamyKL@users.noreply.github.com>
2022-04-22 12:50:14 +0300

    Refactor
    Added comment 187 to improve readability

--- bikeshare (2).py
+++ bikeshare (2).py
@@ -167,7 +167,7 @@ def trip_duration_stats(df):
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
-
+    #To show the time taken to calculate user_stats
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
